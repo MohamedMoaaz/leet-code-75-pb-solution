bd60064d5646532dd8f38c46d3fb70860dd6463d
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-04-01 00:42:05 +0200

    Update Merge Strings Alternately.py

--- Array-String/Merge Strings Alternately.py
+++ Array-String/Merge Strings Alternately.py
@@ -1,7 +1,7 @@
-    def mergeAlternately(self, word1, word2):
-        str1 = ""
-        for i, j in zip(word1, word2):
-            str1 += i
-            str1 += j
-        str1 += word2[len(word1):] if len(word2) > len(word1) else word1[len(word2):]
-        return str1
+def mergeAlternately(self, word1, word2):
+    str1 = ""
+    for i, j in zip(word1, word2):
+        str1 += i
+        str1 += j
+    str1 += word2[len(word1):] if len(word2) > len(word1) else word1[len(word2):]
+    return str1
