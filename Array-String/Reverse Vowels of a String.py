4690d35128b79d13efde58ac4c877d6871a38716
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-04-01 00:42:35 +0200

    Update Reverse Vowels of a String.py

--- Array-String/Reverse Vowels of a String.py
+++ Array-String/Reverse Vowels of a String.py
@@ -1,7 +1,7 @@
-    def reverseVowels(self, s: str) -> str:
-        vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
-        temp = [char for char in s if char in vowels]
-        s = list(s)
-        s = [temp.pop() if char in vowels else char for char in s]
-        s = ''.join(s)
-        return s
+def reverseVowels(self, s: str) -> str:
+    vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
+    temp = [char for char in s if char in vowels]
+    s = list(s)
+    s = [temp.pop() if char in vowels else char for char in s]
+    s = ''.join(s)
+    return s
