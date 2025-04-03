fb2b997de16d7edf5c08fa92d56a87ddffd03984
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-04-03 18:28:21 +0200

    Update Find the Highest Altitude.py

--- Prefix Sum/Find the Highest Altitude.py
+++ Prefix Sum/Find the Highest Altitude.py
@@ -1,1 +1,9 @@
-
+def largestAltitude(self, gain: List[int]) -> int:
+    max_altitude = 0
+    current_altitude = 0
+    
+    for g in gain:
+        current_altitude += g
+        max_altitude = max(max_altitude, current_altitude)
+    
+    return max_altitude
